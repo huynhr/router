2dc329c5afebed6d1f05e3a948fc3d36f95ef103
Raymond Huynh <huynh.r.k@gmail.com>
2017-11-30 11:59:28 -0500

    added routes and PORT

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '/../client/public/')));
 // app.get('/', (req, res) => {
 //   res.send('hello world!')
 // })
-app.listen(3000, () => console.log('listening on port 3000!'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
