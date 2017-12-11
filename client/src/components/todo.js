4f291abb90c6fd9113d8441fb1c11af0a1a47133
Raymond Huynh <huynh.r.k@gmail.com>
2017-12-11 11:31:59 -0500

    Added: 	- able to submit state now

--- client/src/components/todo.js
+++ client/src/components/todo.js
@@ -14,12 +14,20 @@ class Todo extends React.Component {
     this.props.dispatch(addText(e.target.value))
   }
 
+  handleSubmit(e) {
+    alert('Your request has been submited');
+    console.log(this.props.todo.test)
+  }
+
   render() {
     return (
-      <div>
+      <div className="container is-fluid">
+        <div className="notification">
+          This is a container!!!
+        </div>
         <h1>My todo list</h1>
         <p>{this.props.todo.test}</p>
-        <form>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <input  type="text"
                   onChange={this.handleUser.bind(this)}/>
           <input type="submit" value="submit"/>
